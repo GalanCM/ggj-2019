b156b5b575502f613202e59cdf4f77eb2825f78e
Galan Montgomery <galan.c.m@gmail.com>
2019-01-26 13:40:21 -0500

    Add mushroom mechanic

---
+++ Mushroom.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+func _ready() -> void:
+	$Area2D.connect("body_entered", self, "bounce")
+	
+func bounce(body):
+	if (body is Player and body.global_position.y < global_position.y):
+		body.jumping = true
+		body.velocity.y = -400
+		
