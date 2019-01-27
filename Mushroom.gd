5234e10de281dc5110244a81f302cb9f0d68f240
Galan Montgomery <galan.c.m@gmail.com>
2019-01-26 20:19:16 -0500

    Mushroom tweaks.

--- Mushroom.gd
+++ Mushroom.gd
@@ -3,8 +3,19 @@ extends StaticBody2D
 func _ready() -> void:
 	$Area2D.connect("body_entered", self, "bounce")
 	
+func _physics_process(delta: float) -> void:
+	var player = get_tree().get_nodes_in_group("Player")
+	if player.size() == 0:
+		return
+	player = player[0]
+	
+	if $Area2D.overlaps_body(player):
+		$AnimatedSprite.play("squish")
+		yield( $AnimatedSprite, "animation_finished")
+		$AnimatedSprite.play("default")
+	
 func bounce(body):
 	if (body is Player and body.global_position.y < global_position.y):
 		body.jumping = true
-		body.velocity.y = -450
+		body.velocity.y *= -1.5 #-450
 		
