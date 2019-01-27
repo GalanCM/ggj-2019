9670d99f4e47fa9e87f1d51f7c344a3c3b59be4e
raynorj <raynorj@vcu.edu>
2019-01-26 20:15:43 -0500

    Adding lever code and scene

---
+++ Lever.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+onready var start_position = global_position
+
+func _physics_process(delta):
+	var collider = move_and_collide(Vector2(0,0))
+	
+	global_position = start_position
+	
+	if (collider != null and collider.collider is Player):
+		var relative_X = (collider.position-global_position)[0]
+		
+		rotation_degrees = clamp(rotation_degrees+(relative_X*delta),-8,8)
