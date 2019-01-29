083a076ca58a669f906aeab17377c81140309979
Galan Montgomery <galan.c.m@gmail.com>
2019-01-29 14:45:31 -0500

    Prop sfx

--- Lever.gd
+++ Lever.gd
@@ -11,5 +11,9 @@ func _physics_process(delta):
 	
 	if (collider != null and collider.collider is Player):
 		var relative_X = (collider.position-global_position)[0]
+		if not $LeverPlayer.playing:
+			$LeverPlayer.play()
 		
-		rotation_degrees = clamp(rotation_degrees+(relative_X*delta * 0.4),-20,20)
+		rotation_degrees = clamp(rotation_degrees+(relative_X*delta * 0.4),-17,17)
+	elif $LeverPlayer.playing:
+		$LeverPlayer.stop()
--- Mushroom.gd
+++ Mushroom.gd
@@ -12,19 +12,17 @@ func _physics_process(delta: float) -> void:
 		return
 	player = player[0]
 	
-	if $Area2D.overlaps_body(player):
-		$AnimatedSprite.play("squish")
-		yield( $AnimatedSprite, "animation_finished")
-		$AnimatedSprite.play("default")
-	
 func bounce(body):
 	if (body is Player):
 		emit_signal("player_bounce")
+		$BouncePlayer.play()
 
 		if rotation == 0 and body.global_position.y < global_position.y-20:
 			body.velocity.y = min(body.velocity.y * -1.5, -50)
 			body.jumping = true
 		elif body.global_position.y > global_position.y+20:
 			body.velocity.y = max(body.velocity.y * -1.5, 300)
-#			print(body.velocity.reflect(Vector2(0,-1).rotated(rotation) ) * 10)
-#			body.velocity = body.velocity.reflect( Vector2(0,-1).rotated(-rotation) ) * 1
+			
+		$AnimatedSprite.play("squish")
+		yield( $AnimatedSprite, "animation_finished")
+		$AnimatedSprite.play("default")
