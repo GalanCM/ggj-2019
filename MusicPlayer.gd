6601182c0ef68876a59ef962e776449aa8547953
Galan Montgomery <galan.c.m@gmail.com>
2019-01-29 15:08:34 -0500

    Music Crossfade.

--- MusicPlayer.gd
+++ MusicPlayer.gd
@@ -1,5 +1,7 @@
 extends Node
 
 func switch():
-	$Thereof.volume_db = -5
-	$"Lack Thereof".volume_db = -80
+	$"Lack Thereof/Tween".interpolate_method($"Lack Thereof", "set_volume_db", $"Lack Thereof".volume_db, -80, 5, Tween.TRANS_LINEAR, Tween.TRANS_SINE)
+	$Thereof/Tween.interpolate_method($Thereof, "set_volume_db", $Thereof.volume_db, -5, 3, Tween.EASE_IN_OUT, Tween.TRANS_LINEAR)
+	$"Lack Thereof/Tween".start()
+	$Thereof/Tween.start()
